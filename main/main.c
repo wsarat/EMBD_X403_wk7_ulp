2a193674fb12114a0b774c782b7593385d2a5f3c
Sarat W <wsarat@gmail.com>
2023-07-29 15:52:06 -0700

    add serial debug from ulp

--- main/main.c
+++ main/main.c
@@ -2,8 +2,17 @@
 #include <esp_err.h>
 #include "ulp_riscv.h"
 
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
+
+#define UART_PORT   UART_NUM_2
+#define UART_RX_PIN  GPIO_NUM_4     // hard wired to ulp uart tx  
+#define UART_TX_PIN  GPIO_NUM_5     // NC 
+
+#define BUF_SIZE    128
 
 static void init_ulp_program(void)
 {
@@ -20,7 +29,39 @@ static void init_ulp_program(void)
     ESP_ERROR_CHECK(err);
 }
 
+//static void echo_task(void *arg)
+void serialMonitor()
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 9600, // ulp serial baudrate
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    int intr_alloc_flags = 0;
+
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TX_PIN, UART_RX_PIN, -1, -1));
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(UART_PORT, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        // Write data back to the UART
+        if (len)
+            printf("ULP: %s", data);
+    }
+}
+
 void app_main(void)
 {
     init_ulp_program();
+    serialMonitor();
 }
--- main/ulp/ulp_main.c
+++ main/ulp/ulp_main.c
@@ -2,17 +2,39 @@
 #include "ulp_riscv_utils.h"
 #include "ulp_riscv_gpio.h"
 
-#define LED_PIN GPIO_NUM_2
+#include "ulp_riscv_print.h"
+#include "ulp_riscv_uart_ulp_core.h"
+#include "sdkconfig.h" // for UART baudrate
+
+static ulp_riscv_uart_t s_print_uart;
+
+#define LED_PIN     GPIO_NUM_2
+#define UART_TX_PIN GPIO_NUM_14
 
 int main (void)
 {
+    // LED
+    bool led_on = 0;
     ulp_riscv_gpio_init(LED_PIN);
     ulp_riscv_gpio_output_enable(LED_PIN);
 
-    bool led_on = 0;
+    // UART
+    ulp_riscv_uart_cfg_t cfg = {
+        .tx_pin = UART_TX_PIN,
+    };
+
+    ulp_riscv_uart_init(&s_print_uart, &cfg);
+    ulp_riscv_print_install((putc_fn_t)ulp_riscv_uart_putc, &s_print_uart);
+    
+    int count = 0;
+    char buf[32];
     while(1) {
         led_on = !led_on;
         ulp_riscv_gpio_output_level(LED_PIN, led_on);
+
+        ulp_riscv_print_str("ULP-RISCV count: ");
+        ulp_riscv_print_hex(count++);
+        ulp_riscv_print_str("\n");
 
         ulp_riscv_delay_cycles(1000 * ULP_RISCV_CYCLES_PER_MS);
     }
