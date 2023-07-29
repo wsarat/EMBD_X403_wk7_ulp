3864c692daab59fb6ffde788007148fc74dbb1b7
Sarat W <wsarat@gmail.com>
2023-07-29 14:28:21 -0700

    led blink ulp

--- main/main.c
+++ main/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <esp_err.h>
+#include "ulp_riscv.h"
+
+extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
+extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
+
+static void init_ulp_program(void)
+{
+    esp_err_t err = ulp_riscv_load_binary(ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start));
+    ESP_ERROR_CHECK(err);
+
+    /* The first argument is the period index, which is not used by the ULP-RISC-V timer
+     * The second argument is the period in microseconds, which gives a wakeup time period of: 20ms
+     */
+    ulp_set_wakeup_period(0, 20000);
+
+    /* Start the program */
+    err = ulp_riscv_run();
+    ESP_ERROR_CHECK(err);
+}
 
 void app_main(void)
 {
-
+    init_ulp_program();
 }
--- main/ulp/ulp_main.c
+++ main/ulp/ulp_main.c
@@ -1,6 +1,21 @@
+#include <stdbool.h>
+#include "ulp_riscv_utils.h"
+#include "ulp_riscv_gpio.h"
 
+#define LED_PIN GPIO_NUM_2
 
 int main (void)
 {
+    ulp_riscv_gpio_init(LED_PIN);
+    ulp_riscv_gpio_output_enable(LED_PIN);
+
+    bool led_on = 0;
+    while(1) {
+        led_on = !led_on;
+        ulp_riscv_gpio_output_level(LED_PIN, led_on);
+
+        ulp_riscv_delay_cycles(1000 * ULP_RISCV_CYCLES_PER_MS);
+    }
+
     return 0;
 }
