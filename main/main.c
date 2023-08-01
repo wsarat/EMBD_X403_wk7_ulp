5d20368175830276b7b86529f69b3a41f86cf1b6
Sarat W <wsarat@gmail.com>
2023-08-01 01:11:14 -0700

    motion interrupt not working, do some dirty job

--- main/main.c
+++ main/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <esp_err.h>
+#include "esp_sleep.h"
+
 #include "ulp_riscv.h"
 #include "ulp_riscv_i2c.h"
 #include "ulp_riscv_lock.h"
@@ -90,8 +92,7 @@ void rtc_i2c_scan() {
     }
 }
 
-void app_main(void)
-{   
+void init_system() {
     rtc_i2c_init(GPIO_NUM_3, GPIO_NUM_0);
     //rtc_i2c_scan(); // error when change device address
 
@@ -103,21 +104,59 @@ void app_main(void)
     rtc_i2c_read_byte(0x68, 0x75, &data);
     printf("MPU6050 addr: 0x68 register:0x75 (WHO_AM_I) read: 0x%X\n", data);
 
-    xTaskCreate(uart_monitor, "uart_monitor", 1024, NULL, 1, NULL);
     init_ulp_program();
 
-    ulp_riscv_lock_t *lock = (ulp_riscv_lock_t*)&ulp_lock;
+    if (false) {
+        xTaskCreate(uart_monitor, "uart_monitor", 2048, NULL, 1, NULL);
+        ulp_riscv_lock_t *lock = (ulp_riscv_lock_t*)&ulp_lock;
+        while (true) {
+            ulp_riscv_lock_acquire(lock);
+            if (ulp_mpu6050_ready) {
+                printf("Temp = %.02f C ", ((int16_t)ulp_mpu6050_temp)/340 + 35.53);
+                printf("accX = %.02f "  , ((int16_t)ulp_mpu6050_accX)/16384.0);
+                printf("accY = %.02f "  , ((int16_t)ulp_mpu6050_accY)/16384.0);
+                printf("accZ = %.02f \n", ((int16_t)ulp_mpu6050_accZ)/16384.0);
+            }
+            ulp_riscv_lock_release(lock);
 
-    while (true) {
-        ulp_riscv_lock_acquire(lock);
-        if (ulp_mpu6050_ready) {
-            printf("Temp = %.02f C ", ((int16_t)ulp_mpu6050_temp)/340 + 35.53);
-            printf("accX = %.02f ", ((int16_t)ulp_mpu6050_accX)/16384.0);
-            printf("accY = %.02f ", ((int16_t)ulp_mpu6050_accY)/16384.0);
-            printf("accZ = %.02f \n", ((int16_t)ulp_mpu6050_accZ)/16384.0);
+            vTaskDelay(50 / portTICK_PERIOD_MS);
         }
-        ulp_riscv_lock_release(lock);
-
-        vTaskDelay(50 / portTICK_PERIOD_MS);
     }
+}
+
+struct acc_t {
+    float accX;
+    float accY;
+    float accZ;
+};
+
+struct acc_t acc_data[10];
+
+void app_main(void)
+{   
+    esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+    /* not a wakeup from ULP, load the firmware */
+    if (cause != ESP_SLEEP_WAKEUP_ULP) {
+        printf("Normal Start Up (%d).\n", cause);
+        init_system();
+    }
+
+    /* ULP Risc-V read and detected a temperature above the limit */
+    if (cause == ESP_SLEEP_WAKEUP_ULP) {
+        ulp_riscv_lock_t *lock = (ulp_riscv_lock_t*)&ulp_lock;
+        for (int i=0; i<10; i++) {
+            ulp_riscv_lock_acquire(lock);
+            acc_data[i].accX = ((int16_t)ulp_mpu6050_accX)/16384.0;
+            acc_data[i].accY = ((int16_t)ulp_mpu6050_accY)/16384.0;
+            acc_data[i].accZ = ((int16_t)ulp_mpu6050_accZ)/16384.0;
+            ulp_riscv_lock_release(lock);
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+        }
+        printf("Motion Recorded!\n");
+    }
+    /* Go back to sleep, only the ULP Risc-V will run */
+    printf("Entering in deep sleep\n\n");
+    vTaskDelay(100);
+    ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup());
+    esp_deep_sleep_start();
 }
--- main/ulp/ulp_main.c
+++ main/ulp/ulp_main.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdlib.h>
 #include "ulp_riscv.h"
 #include "ulp_riscv_utils.h"
 //#include "ulp_riscv_i2c.h"
@@ -8,14 +9,18 @@
 #include "ulp_riscv_uart_ulp_core.h"
 #include "sdkconfig.h" // for UART baudrate
 
+#include "ulp_riscv.h"
+
 static ulp_riscv_uart_t s_print_uart;
 ulp_riscv_lock_t lock;
+
 
 #define LED_PIN     GPIO_NUM_2
 #define UART_TX_PIN GPIO_NUM_14
 
 #define MPU6050_ADDR    0x68
 
+#define MPU6050_INT          GPIO_NUM_13
 #define MPU6050_SMPLRT_DIV   0x19
 #define MPU6050_CONFIG       0x1a
 #define MPU6050_GYRO_CONFIG  0x1b
@@ -24,6 +29,15 @@ ulp_riscv_lock_t lock;
 #define MPU6050_PWR_MGMT_1   0x6b
 #define MPU6050_TEMP_H       0x41
 #define MPU6050_TEMP_L       0x42
+
+#define SIGNAL_PATH_RESET  0x68
+#define I2C_SLV0_ADDR      0x37
+#define ACCEL_CONFIG       0x1C 
+#define MOT_THR            0x1F  // Motion detection threshold bits [7:0]
+#define MOT_DUR            0x20  // Duration counter threshold for motion interrupt generation, 1 kHz rate, LSB = 1 ms
+#define MOT_DETECT_CTRL    0x69
+#define INT_ENABLE         0x38
+#define INT_STATUS          0x3A
 
 uint16_t mpu6050_ready;
 uint16_t mpu6050_temp;
@@ -89,6 +103,98 @@ void mpu6050_init(uint8_t addr) {
     ulp_i2c_write(addr, MPU6050_GYRO_CONFIG, 0x08);
     ulp_i2c_write(addr, MPU6050_ACCEL_CONFIG, 0x00);
     ulp_i2c_write(addr, MPU6050_PWR_MGMT_1, 0x01);    
+
+    // Interrupt
+    ulp_i2c_write(addr, SIGNAL_PATH_RESET, 0x07); //Reset all internal signal paths in the MPU-6050 by writing 0x07 to register 0x68;
+    ulp_i2c_write(addr, I2C_SLV0_ADDR, 0x20); 
+    //write register 0x37 to select how to use the interrupt pin. For an active high, push-pull signal that stays until register (decimal) 58 is read, write 0x20.
+    ulp_i2c_write(addr, ACCEL_CONFIG, 0x01); 
+    //Write register 28 (==0x1C) to set the Digital High Pass Filter, bits 3:0. For example set it to 0x01 for 5Hz. (These 3 bits are grey in the data sheet, but they are used! Leaving them 0 means the filter always outputs 0.)
+    ulp_i2c_write(addr, MOT_THR, 10); 
+    //Write the desired Motion threshold to register 0x1F (For example, write decimal 20).  
+    ulp_i2c_write(addr, MOT_DUR, 40); 
+    //Set motion detect duration to 1  ms; LSB is 1 ms @ 1 kHz rate  
+    ulp_i2c_write(addr, MOT_DETECT_CTRL, 0x15); 
+    //to register 0x69, write the motion detection decrement and a few other settings (for example write 0x15 to set both free-fall and motion decrements to 1 and accelerometer start-up delay to 5ms total by adding 1ms. )   
+    ulp_i2c_write(addr, INT_ENABLE, 0x40); 
+    //write register 0x38, bit 6 (0x40), to enable motion detection interrupt.     
+    ulp_i2c_write(addr, 0x37, 160); 
+    // now INT pin is active low
+}
+
+uint16_t last_temp = 0;
+uint16_t last_accX = 0;
+uint16_t last_accY = 0;
+uint16_t last_accZ = 0;
+int8_t motion_count = 0;
+
+void read_mpu6050() {
+    uint16_t raw_temp = 0;
+    uint16_t raw_accX = 0;
+    uint16_t raw_accY = 0;
+    uint16_t raw_accZ = 0;
+
+    uint8_t data = 0;
+    uint16_t data2 = 0;
+
+    ulp_i2c_read(MPU6050_ADDR, MPU6050_PWR_MGMT_1, &data);
+    if (data & 1 << 6) {
+        ulp_riscv_print_str("MPU6050 Sleep!!!!\n");
+        return;
+    }
+
+    ulp_i2c_read(MPU6050_ADDR, MPU6050_WHO_AM_I, &data);
+    if (data != 0x68) {
+        ulp_riscv_print_str("something wrong!!!!\n"); 
+        return;
+    }
+    
+    ulp_riscv_gpio_output_level(LED_PIN, 1);
+
+    ulp_i2c_read2(MPU6050_ADDR, MPU6050_TEMP_H, &data2);  
+    raw_temp = data2;  
+
+    ulp_i2c_read2(MPU6050_ADDR, 0x3B, &data2);   
+    raw_accX = data2;       
+
+    ulp_i2c_read2(MPU6050_ADDR, 0x3D, &data2);    
+    raw_accY = data2;   
+
+    ulp_i2c_read2(MPU6050_ADDR, 0x3F, &data2);     
+    raw_accZ = data2;    
+
+    ulp_riscv_gpio_output_level(LED_PIN, 0); 
+
+    // dirty motion lol
+    int sum_diff = 0;
+    sum_diff += abs(last_accX - raw_accX);
+    sum_diff += abs(last_accY - raw_accY);
+    sum_diff += abs(last_accZ - raw_accZ);
+
+    last_accX = raw_accX;
+    last_accY = raw_accY;
+    last_accZ = raw_accZ;
+
+    if (sum_diff > 1000)
+        motion_count += 1;
+    else
+        motion_count = 0;
+
+    if (motion_count > 2) {
+        motion_count = -10;
+        ulp_riscv_print_str("Motion!\n"); 
+        ulp_riscv_wakeup_main_processor();
+    }
+
+    ulp_riscv_lock_acquire(&lock); 
+    mpu6050_ready = 1;
+    mpu6050_temp = raw_temp;
+    mpu6050_accX = raw_accX;
+    mpu6050_accY = raw_accY;
+    mpu6050_accZ = raw_accZ;
+    ulp_riscv_lock_release(&lock); 
+
+    ulp_riscv_delay_cycles(50 * ULP_RISCV_CYCLES_PER_MS);
 }
 
 int main (void)
@@ -106,7 +212,7 @@ int main (void)
     ulp_riscv_uart_init(&s_print_uart, &cfg);
     ulp_riscv_print_install((putc_fn_t)ulp_riscv_uart_putc, &s_print_uart);   
 
-    ulp_riscv_delay_cycles(3000 * ULP_RISCV_CYCLES_PER_MS); // just a delay, not to missed first uart print.
+    ulp_riscv_delay_cycles(1000 * ULP_RISCV_CYCLES_PER_MS); // just a delay, not to missed first uart print.
     ulp_riscv_print_str("ULP riscV started!\n");
 
     // ulp_i2c_scan(); //not working
@@ -114,58 +220,30 @@ int main (void)
     // LED
     ulp_riscv_gpio_init(LED_PIN);
     ulp_riscv_gpio_output_enable(LED_PIN);
-    ulp_riscv_gpio_output_level(LED_PIN, 1);
+    //ulp_riscv_gpio_output_level(LED_PIN, 1);
+
+    // GPIO
+    ulp_riscv_gpio_init(MPU6050_INT);
+    ulp_riscv_gpio_input_enable(MPU6050_INT);
 
     mpu6050_init(MPU6050_ADDR);
 
-    uint8_t data = 0;
-    uint16_t data2 = 0;
+    bool gpio_level;
+    bool gpio_level_previous = (bool)ulp_riscv_gpio_get_level(MPU6050_INT);
+    
+    int count = 0;
     while (true) {
-        ulp_riscv_gpio_output_level(LED_PIN, 1);
-        
-        ulp_i2c_read(MPU6050_ADDR, MPU6050_PWR_MGMT_1, &data);
-        if (data & 1 << 6) {
-            ulp_riscv_print_str("MPU6050 Sleep!!!!\n");
+        gpio_level = (bool)ulp_riscv_gpio_get_level(MPU6050_INT);
+        if (gpio_level != gpio_level_previous) {
+            if (gpio_level == 0) {
+                uart_print("Got Interrupt!", count);
+                count += 1;
+            }
+            gpio_level_previous = gpio_level;
         }
 
-        ulp_i2c_read(MPU6050_ADDR, MPU6050_WHO_AM_I, &data);
-        if (data != 0x68)
-            ulp_riscv_print_str("something wrong!!!!\n");
-        //uart_print("MPU6050_WHO_AM_I", data);
-
-        // ulp_i2c_read(MPU6050_ADDR, MPU6050_TEMP_H, &data);   
-        // uart_print("MPU6050_TEMP_H ", data);
-        // mpu6050_temp = data << 8;
-
-        // ulp_i2c_read(MPU6050_ADDR, MPU6050_TEMP_L, &data);   
-        // uart_print("MPU6050_TEMP_L ", data);   
-        // mpu6050_temp |= data;  
-        ulp_i2c_read2(MPU6050_ADDR, MPU6050_TEMP_H, &data2);  
-        ulp_riscv_lock_acquire(&lock);  
-        mpu6050_temp = data2;  
-        ulp_riscv_lock_release(&lock); 
-
-        ulp_i2c_read2(MPU6050_ADDR, 0x3B, &data2);   
-        ulp_riscv_lock_acquire(&lock);   
-        mpu6050_accX = data2;    
-        ulp_riscv_lock_release(&lock);    
-
-        ulp_i2c_read2(MPU6050_ADDR, 0x3D, &data2);  
-        ulp_riscv_lock_acquire(&lock);    
-        mpu6050_accY = data2; 
-        ulp_riscv_lock_release(&lock);   
-
-        ulp_i2c_read2(MPU6050_ADDR, 0x3F, &data2);    
-        ulp_riscv_lock_acquire(&lock);  
-        mpu6050_accZ = data2;   
-        ulp_riscv_lock_release(&lock);   
-
-        ulp_riscv_gpio_output_level(LED_PIN, 0);
-        ulp_riscv_lock_acquire(&lock); 
-        mpu6050_ready = 1;
-        ulp_riscv_lock_release(&lock);
-
-        ulp_riscv_delay_cycles(50 * ULP_RISCV_CYCLES_PER_MS);
+        read_mpu6050();
+        //ulp_riscv_delay_cycles(50 * ULP_RISCV_CYCLES_PER_MS);
     }
 
     return 0;
