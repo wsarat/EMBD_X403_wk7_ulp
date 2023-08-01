401ea3ea063f6a62324d2ebb7dc54ca5ab8a2a8b
Sarat W <wsarat@gmail.com>
2023-07-31 22:18:37 -0700

    ulp i2c workgit add --all

--- main/main.c
+++ main/main.c
@@ -2,9 +2,12 @@
 #include <esp_err.h>
 #include "ulp_riscv.h"
 #include "ulp_riscv_i2c.h"
+#include "ulp_riscv_lock.h"
 
 #include "driver/gpio.h"
 #include "driver/uart.h"
+
+#include "ulp_main.h"
 
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
@@ -12,29 +15,20 @@ extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
 #define UART_PORT   UART_NUM_2
 #define UART_RX_PIN  GPIO_NUM_4     // hard wired to ulp uart tx  
 #define UART_TX_PIN  GPIO_NUM_5     // NC 
-
-#define BUF_SIZE    128
+#define UART_BUF_SIZE    128
 
 static void init_ulp_program(void)
 {
     esp_err_t err = ulp_riscv_load_binary(ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start));
     ESP_ERROR_CHECK(err);
 
-    /* The first argument is the period index, which is not used by the ULP-RISC-V timer
-     * The second argument is the period in microseconds, which gives a wakeup time period of: 20ms
-     */
     ulp_set_wakeup_period(0, 20000);
-
-    /* Start the program */
     err = ulp_riscv_run();
     ESP_ERROR_CHECK(err);
 }
 
-//static void echo_task(void *arg)
-void serialMonitor()
+static void uart_monitor(void *arg)
 {
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
     uart_config_t uart_config = {
         .baud_rate = 9600, // ulp serial baudrate
         .data_bits = UART_DATA_8_BITS,
@@ -45,16 +39,16 @@ void serialMonitor()
     };
     int intr_alloc_flags = 0;
 
-    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, UART_BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
     ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TX_PIN, UART_RX_PIN, -1, -1));
 
     // Configure a temporary buffer for the incoming data
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    uint8_t *data = (uint8_t *) malloc(UART_BUF_SIZE);
 
     while (1) {
         // Read data from the UART
-        int len = uart_read_bytes(UART_PORT, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        int len = uart_read_bytes(UART_PORT, data, (UART_BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
         // Write data back to the UART
         data[len] = 0x0;
         if (len)
@@ -62,22 +56,68 @@ void serialMonitor()
     }
 }
 
-void app_main(void)
-{   
+void rtc_i2c_init(uint8_t sda_pin, u_int8_t scl_pin) {
+    printf("Initializing RTC I2C ...\n");
+
     ulp_riscv_i2c_cfg_t i2c_cfg = {
         ULP_RISCV_I2C_DEFAULT_GPIO_CONFIG()
-        ULP_RISCV_I2C_FAST_MODE_CONFIG()  
-        //ULP_RISCV_I2C_STANDARD_MODE_CONFIG()
+        ULP_RISCV_I2C_FAST_MODE_CONFIG()
     };
-    i2c_cfg.i2c_pin_cfg.sda_io_num = GPIO_NUM_3;
-    i2c_cfg.i2c_pin_cfg.scl_io_num = GPIO_NUM_0;
+    i2c_cfg.i2c_pin_cfg.sda_io_num = sda_pin;
+    i2c_cfg.i2c_pin_cfg.scl_io_num = scl_pin;
 
-    esp_err_t ret = ulp_riscv_i2c_master_init(&i2c_cfg);
-    if (ret!= ESP_OK) {
-        printf("ERROR: Failed to initialize RTC I2C. Aborting...\n");
-        abort();
+    printf("RTC i2c SDA pin: %ld, SCL pin: %ld Fast Mode.\n",
+        i2c_cfg.i2c_pin_cfg.sda_io_num,
+        i2c_cfg.i2c_pin_cfg.scl_io_num);
+    if (ulp_riscv_i2c_master_init(&i2c_cfg) != ESP_OK)
+        printf("RTC i2c init failed.\n");
+}
+
+void rtc_i2c_read_byte(uint8_t addr, uint8_t reg_addr, u_int8_t *data) {
+    ulp_riscv_i2c_master_set_slave_addr(addr);
+    ulp_riscv_i2c_master_set_slave_reg_addr(reg_addr);
+    ulp_riscv_i2c_master_read_from_device(data, 1);
+}
+
+void rtc_i2c_scan() {
+    for (int addr=0; addr<0x7F; addr++) {
+        if (addr % 8 == 0)
+            printf("\n");
+
+        uint8_t data = 0;
+        rtc_i2c_read_byte(addr, 0x00, &data);
+        printf("0x%02X ", data);
     }
+}
 
+void app_main(void)
+{   
+    rtc_i2c_init(GPIO_NUM_3, GPIO_NUM_0);
+    //rtc_i2c_scan(); // error when change device address
+
+    uint8_t data = 0;
+    rtc_i2c_read_byte(0x68, 0x75, &data);
+    printf("MPU6050 addr: 0x68 register:0x75 (WHO_AM_I) read: 0x%X <-- First time mostly failed.\n", data);
+    // https://github.com/espressif/esp-idf/issues/11608
+
+    rtc_i2c_read_byte(0x68, 0x75, &data);
+    printf("MPU6050 addr: 0x68 register:0x75 (WHO_AM_I) read: 0x%X\n", data);
+
+    xTaskCreate(uart_monitor, "uart_monitor", 1024, NULL, 1, NULL);
     init_ulp_program();
-    serialMonitor();
+
+    ulp_riscv_lock_t *lock = (ulp_riscv_lock_t*)&ulp_lock;
+
+    while (true) {
+        ulp_riscv_lock_acquire(lock);
+        if (ulp_mpu6050_ready) {
+            printf("Temp = %.02f C ", ((int16_t)ulp_mpu6050_temp)/340 + 35.53);
+            printf("accX = %.02f ", ((int16_t)ulp_mpu6050_accX)/16384.0);
+            printf("accY = %.02f ", ((int16_t)ulp_mpu6050_accY)/16384.0);
+            printf("accZ = %.02f \n", ((int16_t)ulp_mpu6050_accZ)/16384.0);
+        }
+        ulp_riscv_lock_release(lock);
+
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
 }
--- main/ulp/ulp_main.c
+++ main/ulp/ulp_main.c
@@ -1,112 +1,171 @@
 #include <stdbool.h>
+#include "ulp_riscv.h"
 #include "ulp_riscv_utils.h"
-#include "ulp_riscv_gpio.h"
+//#include "ulp_riscv_i2c.h"
+#include "ulp_riscv_lock_ulp_core.h"
 
 #include "ulp_riscv_print.h"
 #include "ulp_riscv_uart_ulp_core.h"
 #include "sdkconfig.h" // for UART baudrate
 
-#include "ulp_riscv_i2c_ulp_core.h"
-
 static ulp_riscv_uart_t s_print_uart;
+ulp_riscv_lock_t lock;
 
 #define LED_PIN     GPIO_NUM_2
 #define UART_TX_PIN GPIO_NUM_14
 
 #define MPU6050_ADDR    0x68
-#define PWR_MGMT_1     0x6B
-#define WHO_AM_I        117
 
-void uart_print(char* key, uint8_t value) {
+#define MPU6050_SMPLRT_DIV   0x19
+#define MPU6050_CONFIG       0x1a
+#define MPU6050_GYRO_CONFIG  0x1b
+#define MPU6050_ACCEL_CONFIG 0x1c
+#define MPU6050_WHO_AM_I     0x75
+#define MPU6050_PWR_MGMT_1   0x6b
+#define MPU6050_TEMP_H       0x41
+#define MPU6050_TEMP_L       0x42
+
+uint16_t mpu6050_ready;
+uint16_t mpu6050_temp;
+uint16_t mpu6050_accX;
+uint16_t mpu6050_accY;
+uint16_t mpu6050_accZ;
+
+void uart_print(char* key, uint16_t value) {
     ulp_riscv_print_str(key);
     ulp_riscv_print_str(" -> ");
     ulp_riscv_print_hex(value);
     ulp_riscv_print_str("\n");
 }
 
-void read_i2c(int8_t i2c_addr, int8_t i2c_reg_addr, int8_t *data)
+void ulp_i2c_read(int8_t addr, int8_t reg_addr, int8_t *data)
 {
-    /* Set slave register address to the control register */
-    ulp_riscv_i2c_master_set_slave_addr(i2c_addr);
-    ulp_riscv_i2c_master_set_slave_reg_addr(i2c_reg_addr);
+    ulp_riscv_i2c_master_set_slave_addr(addr);
+    ulp_riscv_i2c_master_set_slave_reg_addr(reg_addr);
     ulp_riscv_i2c_master_read_from_device(data, 1);
 }
 
-void write_i2c(int8_t i2c_addr, int8_t i2c_reg_addr, int8_t *data)
+void ulp_i2c_read2(int8_t addr, int8_t reg_addr, int16_t *data)
 {
-    /* Set slave register address to the control register */
-    ulp_riscv_i2c_master_set_slave_addr(i2c_addr);
-    ulp_riscv_i2c_master_set_slave_reg_addr(i2c_reg_addr);
-    ulp_riscv_i2c_master_write_to_device(data, 1);
+    uint8_t temp = 0;
+    ulp_riscv_i2c_master_set_slave_addr(addr);
+    ulp_riscv_i2c_master_set_slave_reg_addr(reg_addr);
+    ulp_riscv_i2c_master_read_from_device(&temp, 1);
+    *data = temp << 8;
+
+    ulp_riscv_i2c_master_set_slave_reg_addr(reg_addr+1);
+    ulp_riscv_i2c_master_read_from_device(&temp, 1);
+    *data |= temp;
 }
 
-void scan_i2c() {
+void ulp_i2c_write(int8_t addr, int8_t reg_addr, int8_t data)
+{
+    ulp_riscv_i2c_master_set_slave_addr(addr);
+    ulp_riscv_i2c_master_set_slave_reg_addr(reg_addr);
+    ulp_riscv_i2c_master_write_to_device(&data, 1);
+}
+
+void ulp_i2c_scan() {
     int ret;
     uint8_t data;
 
-    ulp_riscv_print_str("i2c scan start.\n");
+    ulp_riscv_print_str("ulp i2c scan start.\n");
     
-    for (uint8_t addr=0; addr<0x7f; addr++) {     
+    for (uint8_t addr=0; addr<0x7f; addr++) {  
         data = 0;
-        read_i2c(addr, 0, &data);
-        if (data > 0) {
-            uart_print("Found i2c at Address", addr);
-            uart_print("Read", data);
-            break;
-        }
+        ulp_i2c_read(addr, 0, &data);
+        if (data > 0)
+            uart_print("Found i2c at ", addr);
     }    
-    ulp_riscv_print_str("i2c scan end.\n\n");           
+    ulp_riscv_print_str("ulp i2c scan end.\n\n");           
 }
 
-void mpu6050_init() {
-    ulp_riscv_print_str("mpu6050 init (write PWR_MGMT_1 = 0)\n");
-    //write_i2c(MPU6050_ADDR, PWR_MGMT_1, 0);
-    uint8_t data = 0;
-    write_i2c(MPU6050_ADDR, PWR_MGMT_1, &data);
-    write_i2c(0x6A, PWR_MGMT_1, &data);
+void mpu6050_init(uint8_t addr) {
+    ulp_riscv_print_str("mpu6050 init.\n");
+    
+    ulp_i2c_write(addr, MPU6050_PWR_MGMT_1, 0);
+    ulp_i2c_write(addr, MPU6050_SMPLRT_DIV, 0x00);
+    ulp_i2c_write(addr, MPU6050_CONFIG, 0x00);
+    ulp_i2c_write(addr, MPU6050_GYRO_CONFIG, 0x08);
+    ulp_i2c_write(addr, MPU6050_ACCEL_CONFIG, 0x00);
+    ulp_i2c_write(addr, MPU6050_PWR_MGMT_1, 0x01);    
 }
 
 int main (void)
-{
-    // LED
-    bool led_on = 0;
-    ulp_riscv_gpio_init(LED_PIN);
-    ulp_riscv_gpio_output_enable(LED_PIN);
+{   
+    mpu6050_ready = 0;
+    mpu6050_temp = 0;
+    mpu6050_accX = 0;
+    mpu6050_accY = 0;
+    mpu6050_accZ = 0;
 
     // UART
     ulp_riscv_uart_cfg_t cfg = {
         .tx_pin = UART_TX_PIN,
     };
-
     ulp_riscv_uart_init(&s_print_uart, &cfg);
-    ulp_riscv_print_install((putc_fn_t)ulp_riscv_uart_putc, &s_print_uart);
-    
-    char buf[32];
-    int8_t data;
+    ulp_riscv_print_install((putc_fn_t)ulp_riscv_uart_putc, &s_print_uart);   
 
-    ulp_riscv_delay_cycles(3000 * ULP_RISCV_CYCLES_PER_MS);
-    mpu6050_init();
-    ulp_riscv_delay_cycles(1000 * ULP_RISCV_CYCLES_PER_MS);
+    ulp_riscv_delay_cycles(3000 * ULP_RISCV_CYCLES_PER_MS); // just a delay, not to missed first uart print.
+    ulp_riscv_print_str("ULP riscV started!\n");
 
-    scan_i2c();
+    // ulp_i2c_scan(); //not working
 
-    ulp_riscv_print_str("mpu6050 at 0x68\n");
-    data = 0;
-    read_i2c(MPU6050_ADDR, WHO_AM_I, &data);
-    uart_print("WHO_AM_I", data);
-    ulp_riscv_print_str("\n");
+    // LED
+    ulp_riscv_gpio_init(LED_PIN);
+    ulp_riscv_gpio_output_enable(LED_PIN);
+    ulp_riscv_gpio_output_level(LED_PIN, 1);
 
-    ulp_riscv_print_str("mpu6050 at 0x6a\n");
-    data = 0;
-    read_i2c(0x6a, WHO_AM_I, &data);
-    uart_print("WHO_AM_I", data);
-    ulp_riscv_print_str("\n");
+    mpu6050_init(MPU6050_ADDR);
 
-    while(1) {
-        led_on = !led_on;
-        ulp_riscv_gpio_output_level(LED_PIN, led_on);
+    uint8_t data = 0;
+    uint16_t data2 = 0;
+    while (true) {
+        ulp_riscv_gpio_output_level(LED_PIN, 1);
+        
+        ulp_i2c_read(MPU6050_ADDR, MPU6050_PWR_MGMT_1, &data);
+        if (data & 1 << 6) {
+            ulp_riscv_print_str("MPU6050 Sleep!!!!\n");
+        }
 
-        ulp_riscv_delay_cycles(1000 * ULP_RISCV_CYCLES_PER_MS);
+        ulp_i2c_read(MPU6050_ADDR, MPU6050_WHO_AM_I, &data);
+        if (data != 0x68)
+            ulp_riscv_print_str("something wrong!!!!\n");
+        //uart_print("MPU6050_WHO_AM_I", data);
+
+        // ulp_i2c_read(MPU6050_ADDR, MPU6050_TEMP_H, &data);   
+        // uart_print("MPU6050_TEMP_H ", data);
+        // mpu6050_temp = data << 8;
+
+        // ulp_i2c_read(MPU6050_ADDR, MPU6050_TEMP_L, &data);   
+        // uart_print("MPU6050_TEMP_L ", data);   
+        // mpu6050_temp |= data;  
+        ulp_i2c_read2(MPU6050_ADDR, MPU6050_TEMP_H, &data2);  
+        ulp_riscv_lock_acquire(&lock);  
+        mpu6050_temp = data2;  
+        ulp_riscv_lock_release(&lock); 
+
+        ulp_i2c_read2(MPU6050_ADDR, 0x3B, &data2);   
+        ulp_riscv_lock_acquire(&lock);   
+        mpu6050_accX = data2;    
+        ulp_riscv_lock_release(&lock);    
+
+        ulp_i2c_read2(MPU6050_ADDR, 0x3D, &data2);  
+        ulp_riscv_lock_acquire(&lock);    
+        mpu6050_accY = data2; 
+        ulp_riscv_lock_release(&lock);   
+
+        ulp_i2c_read2(MPU6050_ADDR, 0x3F, &data2);    
+        ulp_riscv_lock_acquire(&lock);  
+        mpu6050_accZ = data2;   
+        ulp_riscv_lock_release(&lock);   
+
+        ulp_riscv_gpio_output_level(LED_PIN, 0);
+        ulp_riscv_lock_acquire(&lock); 
+        mpu6050_ready = 1;
+        ulp_riscv_lock_release(&lock);
+
+        ulp_riscv_delay_cycles(50 * ULP_RISCV_CYCLES_PER_MS);
     }
 
     return 0;
